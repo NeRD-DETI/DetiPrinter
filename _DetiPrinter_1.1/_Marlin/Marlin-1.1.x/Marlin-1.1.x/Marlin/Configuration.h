26d80190830092cc2c8ec74c6436cf2c0c64d0cd
Pedro Nuno Ferreira Magalhães <pnfmagalhaes@gmail.com>
2018-06-07 19:49:28 +0100

    z_axis

--- _DetiPrinter_1.1/_Marlin/Marlin-1.1.x/Marlin-1.1.x/Marlin/Configuration.h
+++ _DetiPrinter_1.1/_Marlin/Marlin-1.1.x/Marlin-1.1.x/Marlin/Configuration.h
@@ -529,7 +529,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 8000, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
