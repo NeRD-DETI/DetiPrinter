9a00d827ff8702b0646959d78b0b7102510bd8e1
Pedro Nuno Ferreira Magalhães <pnfmagalhaes@gmail.com>
2018-06-11 15:38:14 +0100

    Marlin
    Inversão do coreXY e ajuste dos passos do extruder1

--- _DetiPrinter_1.1/_Marlin/Marlin-1.1.x/Marlin-1.1.x/Marlin/Configuration.h
+++ _DetiPrinter_1.1/_Marlin/Marlin-1.1.x/Marlin-1.1.x/Marlin/Configuration.h
@@ -529,7 +529,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 8000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 6400, 837 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -749,8 +749,8 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Y_DIR false
+#define INVERT_Z_DIR true
 
 // Enable this option for Toshiba stepper drivers
 //#define CONFIG_STEPPERS_TOSHIBA
